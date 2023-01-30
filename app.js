3ce0f6987463223474324f61af09840a7853f705
Tejas <tejasgiri910@gmail.com>
2023-01-30 22:34:44 +0530

    corse policy fixed

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 import course from "./routes/CourseRoutes.js";
 import user from "./routes/userRoutes.js";
