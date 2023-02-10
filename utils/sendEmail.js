7caeb87e5b3d9b027f1a387c01fc87f7b8c91be1
Tejas <tejasgiri910@gmail.com>
2023-02-10 19:08:39 +0530

    updated gmail service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,28 @@
 import { createTransport } from "nodemailer";
 
+// export const sendEmail = async (to, subject, text) => {
+//   const transporter = createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: process.env.SMTP_PORT,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//   });
+//   await transporter.sendMail({
+//     to,
+//     subject,
+//     text,
+//   });
+// };
+
 export const sendEmail = async (to, subject, text) => {
   const transporter = createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    service: "gmail",
+
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASSWORD,
     },
   });
   await transporter.sendMail({
