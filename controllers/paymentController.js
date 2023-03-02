7890456544a9c4fd39220490d3bd451ffa0c8b07
Tejas <tejasgiri910@gmail.com>
2023-03-02 23:22:56 +0530

    fixed bug of razorpay

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -68,30 +68,44 @@ export const getRazorpayKey = catchAsyncError(async (req, res, next) => {
 export const cancelSubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
   const subscriptionId = user.subscription.id;
-
-  let refund = false;
-  await instance.subscriptions.cancel(subscriptionId);
-
-  const payment = await Payment.findOne({
-    razorpay_subscription_id: subscriptionId,
-  });
-
-  const gap = Date.now() - payment.createdAt;
-  const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
-  if (refundTime > gap) {
-    await instance.payments.refund(payment.razorpay_payment_id);
-    refund = true;
+  const status = user.subscription.status;
+  if (!subscriptionId) {
+    return new ErrorHandler("No subscription id found", 404);
   }
 
-  await payment.remove();
-  user.subscription.id = undefined;
-  user.subscription.status = undefined;
+  if (!status) {
+    return new ErrorHandler("Your subscription is not active", 404);
+  }
 
-  await user.save();
-  res.status(200).json({
-    success: true,
-    message: refund
-      ? "subscription canceled successfully. You will receive your payment within 7 days"
-      : "subscription canceled, no refund initiated as subscription was cancelled after 7 days.",
-  });
+  let refund = false;
+  try {
+    const cancelledPayment = await instance.subscriptions.cancel(
+      subscriptionId
+    );
+
+    const payment = await Payment.findOne({
+      razorpay_subscription_id: subscriptionId,
+    });
+
+    const gap = Date.now() - payment.createdAt;
+    const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
+    if (refundTime > gap) {
+      await instance.payments.refund(payment.razorpay_payment_id);
+      refund = true;
+    }
+
+    await payment.remove();
+    user.subscription.id = undefined;
+    user.subscription.status = undefined;
+
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: refund
+        ? "subscription canceled successfully. You will receive your payment within 7 days"
+        : "subscription canceled, no refund initiated as subscription was cancelled after 7 days.",
+    });
+  } catch (error) {
+    return new ErrorHandler(error.message, 500);
+  }
 });
